e7f4b75e4568d5cee16b5e2d1ba7498d8236a1ee
ogawaminori <minori.ogawa@rakus-partners.co.jp>
2026-06-17 15:33:59 +0900

    ADMIN登録処理-DB接続完了

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -3,8 +3,10 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 
@@ -16,8 +18,18 @@ public class AdministratorController {
     private AdministratorService administratorService;
 
     @GetMapping("/toInsert")
-    public String toInsert(InsertAdministratorForm form){
+    public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert";
     }
-    
+
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        Administrator administrator = new Administrator();
+        administrator.setName(form.getName());
+        administrator.setMailAddress(form.getMailAddress());
+        administrator.setPassword(form.getPassword());
+        administratorService.insert(administrator);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 /**
- * 管理者用Serviceクラス
+ * 管理者情報を登録する業務処理を⾏うメソッドを作成する 
  */
 @Service
 @Transactional
@@ -16,4 +17,7 @@ public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository;
     
+    public void insert(Administrator administrator){
+        administratorRepository.insert(administrator);
+    }
 }
